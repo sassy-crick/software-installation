93fcfaf85e895ec8a011af2dbf260107098d973d
Sassy <sassy-work@formativ.net>
2021-11-09 20:19:30 +0000

    Original commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# software-installation
-Repository for automatic software installation with a Singularity container containing EasyBuild. 
+# Automatic software installation script
+
+
+
+## Introduction:
+
+The aim of the script is to install the software inside a container, and thus the so installed software is independent from the OS as much as possible. The idea comes from the EESSI project and how the software is installed in there. So kudos to them!
+
+## How-to:
+
+Before the script can run, there are a few files which need to be adjusted. 
+
+- `install.sh`
+- `softwarelist.txt`
+
+The `install.sh` does basically the whole magic. There are a few lines at the top which need to be changed to reflect where the software needs to go. The most important are:
+
+- `SOFTWARE_HOME` which is where the software tree and all the helper stuff lives
+- `BINDDIR` is the directory which needs to be bound inside the container as per default Singularity does only mount `/tmp` and `/home` it seems.
+
+You also might want to look at:
+- `CONTAINER_VERSION` which is the name of the sif-file, i.e. the container
+- `EB_VERSION` which is the version of EasyBuild to be used for building software. If that does not exist, it should be automatically installed
+- `SW_LIST` contains a simple list of the EasyConfig files to be installed. All in one line with a blank between them. 
+
+The `SW_LIST` might need to be changed later to an EasyStack file. 
+
+The `software.sh` will be created on the fly in the right directory and  does contain the list of software which needs to be installed. 
+
+The first time the script runs, it will create the directory structure but then stops as the Singularity container is not in place. For the full automated installation, we would download the container from somewhere. However, as this only needs to be done once, it is left for now like this.
+
+Once the container in the right folder we are upgrading EasyBuild to the latest version. This way, a module file is created automatically. Once that is done, the software will be installed if required.  
+
+## Requirements:
+
+`Singularity` >= 2.7.x and `fusermount` >= 2.9.7
+
+
+## To Do:
+
+It needs to be tested on Lustre and/or Ceph as well but that does currently not work as `fusermount` on Rosalind is too old.
+
---
+++ scripts/install.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+# Script for the automatic installation of software
+# We are using a singularity container for the installation
+# so we are isolated from the OS
+# This script does the actual installation.
+# The software stack, so to speak will be in the 
+# software.sh script. That will be replaced with a yml file
+# later for production. 
+# 28/10/2021: initial testing
+# 09/11/2921: some tidy up
+
+# Some defaults
+
+# We need to set the right paths first.
+# We do that via variables up here:
+# These are the only bits which need to be modified:
+# Where to install the software:
+SOFTWARE_HOME="/scr/apps"
+# SOFTWARE_HOME="/scratch/users/$(whoami)"
+# Which container name to be used:
+CONTAINER_VERSION="eb-4.4.1-Lmod-ubuntu20-LTR-2.sif"
+# Which EasyBuild version to be used for the software installation:
+EB_VERSION="4.5.0"
+# Where is the list of the software to be installed:
+SW_LIST=$(cat ${SOFTWARE_HOME}/softwarelist.txt)
+# We might need to bind an additional external directory into the container:
+BINDDIR="/scr:/scr"
+
+# We need to export them so we can modify the template for the software to be installed
+export EB_VERSION
+export SW_LIST
+
+#########################################################################################
+# These should not need to be touched
+OVERLAY_BASEDIR=${SOFTWARE_HOME}/software
+OVERLAY_LOWERDIR=${OVERLAY_BASEDIR}/lower
+OVERLAY_UPPERDIR=${OVERLAY_BASEDIR}/apps
+OVERLAY_WORKDIR=${OVERLAY_BASEDIR}/work
+OVERLAY_MOUNTPOINT="/app"
+CONTAINER_DIR=${OVERLAY_BASEDIR}/containers
+CONTAINER=${CONTAINER_DIR}/${CONTAINER_VERSION}
+SCRIPTS_DIR=${OVERLAY_BASEDIR}/scripts
+SOFTWARE=${SCRIPTS_DIR}/"software.sh"
+#########################################################################################
+
+echo "Installation started at $(date)"
+
+# We check if the folders are here, if not we install them
+if [ -d ${SOFTWARE_HOME} ]; then
+	echo "Making sure all direcotries exist in ${SOFTWARE_HOME} "
+	mkdir -p ${OVERLAY_BASEDIR}/{lower,apps,work,containers,scripts}
+else
+	echo "It appears that ${SOFTWARE_HOME} does not exist"
+	echo "Please make sure the provided path is correct and make the required directory"
+	echo "Bombing out here."
+	exit 2
+fi
+
+# We check if the singularity container exists
+if [ ! -f ${CONTAINER} ]; then
+	echo "The Singularity Container ${CONTAINER} does not exist!"
+	echo "Please install the container before you can continue."
+	echo "Bombing out here."
+	exit 2
+fi
+
+# We create the software.sh file on the fly in the right place. Any previous version will be removed.
+rm -f ${SOFTWARE}
+envsubst '${EB_VERSION},${SW_LIST}' < /scr/build/software/scripts/software.tmpl > ${SOFTWARE} 
+chmod a+x ${SOFTWARE}
+
+# We check if we got the fuse-overly installed and if not, install it
+if [ ! -f ${OVERLAY_BASEDIR}/fuse-overlayfs ]; then
+	 wget -O ${OVERLAY_BASEDIR}/fuse-overlayfs https://github.com/containers/fuse-overlayfs/releases/download/v1.7.1/fuse-overlayfs-x86_64
+	 chmod a+x ${OVERLAY_BASEDIR}/fuse-overlayfs  
+fi
+
+# We check if we already have an EasyBuild module file.
+# If there is none, we assume it is a fresh installation and so we need
+# to upgrade EasyBuild to the latest version first before we can continue
+if [ ! -d ${OVERLAY_UPPERDIR}/modules/all/EasyBuild ]; then
+		singularity exec --bind ${BINDDIR} --fusemount "container:${OVERLAY_BASEDIR}/fuse-overlayfs -o lowerdir=${OVERLAY_LOWERDIR} \
+		-o upperdir=${OVERLAY_UPPERDIR} -o workdir=${OVERLAY_WORKDIR} ${OVERLAY_MOUNTPOINT}" ${CONTAINER} eb --install-latest-eb-release
+	elif [ ! -e ${OVERLAY_UPPERDIR}/modules/all/EasyBuild/${EB_VERSION}.lua ]; then
+	       	echo "We are upgrading EasyBuild to the latest version and then stop."
+		# We can execute the container and tell it what to do:
+		singularity exec --bind ${BINDDIR} --fusemount "container:${OVERLAY_BASEDIR}/fuse-overlayfs -o lowerdir=${OVERLAY_LOWERDIR} \
+		-o upperdir=${OVERLAY_UPPERDIR} -o workdir=${OVERLAY_WORKDIR} ${OVERLAY_MOUNTPOINT}" ${CONTAINER} eb --install-latest-eb-release
+fi
+
+# If the directory exist and the latest EasyBuild module is there, we simply install the
+# software stack which we provide. 
+if [ -e ${OVERLAY_UPPERDIR}/modules/all/EasyBuild/${EB_VERSION}.lua ]; then
+	echo "We are installing the software as defined in ${SOFTWARE}"
+	# We can execute the container and tell it what to do:
+ 	singularity exec --bind ${BINDDIR} --fusemount "container:${OVERLAY_BASEDIR}/fuse-overlayfs -o lowerdir=${OVERLAY_LOWERDIR} \
+	-o upperdir=${OVERLAY_UPPERDIR} -o workdir=${OVERLAY_WORKDIR} ${OVERLAY_MOUNTPOINT}" ${CONTAINER} ${SOFTWARE}
+fi
+
+echo "Installation finished at $(date)"
+
